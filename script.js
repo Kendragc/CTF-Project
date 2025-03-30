2be05af23d975800308607b4e1fee0ac13f55b4a
Kendragc <144168280+Kendragc@users.noreply.github.com>
2025-03-30 17:47:04 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-const decoy = "U3VwZXJfTm90X1RoZV9GbGFn"; // Decodes to: Super_Not_The_Flag
+const d = "U3VwZXJfTm90X1RoZV9GbGFn";
+const e = "Q1RGeydHMDBkX0owOF95MHVf"; 
 
-const e = "Q1RGeydHMDBkX0owOF95MHVf"; // CTF{'G00d_J08_y0u_
 console.log("Need help? Base64 can be decoded, but not all clues are real...");
 
-document.cookie = "mislead=U29ycnlfbm90X3RoaXNfb25l"; // Sorry_not_this_one
+document.cookie = "mislead=U29ycnlfbm90X3RoaXNfb25l"; 
 
 setTimeout(() => {
   document.cookie = "ctfHint=sbhaQ_gur3_s14T!}"; // rot13 -> found_the3_f14G!}
-}, 3000); // Delayed to make things more interesting
+}, 3000); 
 
 // Obfuscated local storage logic
 (function () {
